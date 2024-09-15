d62b54bcad183160ad50141911fda4668e942820
csi78 <4oz.developer@gmail.com>
2024-09-16 11:45:19 +1200

    implement max function

--- signMag.c
+++ signMag.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+
 #include "signMag.h"
 
 SignMag_t signMag_init(bool isNegative, uint16_t magnitude)
@@ -106,3 +107,75 @@ SignMag_t signMag_accumulate(const SignMag_t* array, size_t arraySize)
 	return result;
 }
 
+bool signMag_max(SignMag_t* num1, SignMag_t* num2, SignMag_t** max)
+{
+	if (num1->magnitude == 65535) {
+		*max = num1;
+		return true;
+	}
+	if (num2->magnitude == 65535) {
+		*max = num2;
+		return true;
+	}
+	if (num1->isNegative && !num2->isNegative) {
+		*max = num2;
+		return true;
+	} else if (!num1->isNegative && num2->isNegative) {
+		*max = num1;
+		return true;
+	} else if (!num1->isNegative && !num2->isNegative) {
+		if (num1->magnitude >= num2->magnitude) {
+			*max = num1;
+		} else {
+			*max = num2;
+		}
+		return true;
+	} else if (num1->isNegative && num2->isNegative) {
+		if (num1->magnitude >= num2->magnitude) {
+			*max = num2;
+		} else {
+			*max = num1;
+		}
+		return true;
+	}
+	return false;
+}
+int main(void) {
+    // Define some SignMag_t values for testing
+    SignMag_t num1 = signMag_init(true, 30000);  // -30000
+    SignMag_t num2 = signMag_init(false, 20000); // 20000
+    SignMag_t num3 = signMag_init(true, 60000);  // -60000
+    SignMag_t num4 = signMag_init(false, 65535); // 65535
+    
+    // Pointers to hold the maximum values
+    SignMag_t* max;
+
+    // Test 1: Comparing -30000 and 20000
+    if (signMag_max(&num1, &num2, &max)) {
+        printf("Max of %d and %d is: ", num1.isNegative ? -num1.magnitude : num1.magnitude, num2.isNegative ? -num2.magnitude : num2.magnitude);
+        signMag_print(*max);
+    } else {
+        printf("Error in finding max.\n");
+    }
+
+    // Test 2: Comparing -60000 and 65535
+    if (signMag_max(&num3, &num4, &max)) {
+        printf("Max of %d and %d is: ", num3.isNegative ? -num3.magnitude : num3.magnitude, num4.isNegative ? -num4.magnitude : num4.magnitude);
+        signMag_print(*max);
+    } else {
+        printf("Error in finding max.\n");
+    }
+
+    // Test 3: Comparing two negative numbers
+    SignMag_t num5 = signMag_init(true, 40000); // -40000
+    SignMag_t num6 = signMag_init(true, 45000); // -45000
+
+    if (signMag_max(&num5, &num6, &max)) {
+        printf("Max of %d and %d is: ", num5.isNegative ? -num5.magnitude : num5.magnitude, num6.isNegative ? -num6.magnitude : num6.magnitude);
+        signMag_print(*max);
+    } else {
+        printf("Error in finding max.\n");
+    }
+
+    return 0;
+}
